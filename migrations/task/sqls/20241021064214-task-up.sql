e8b000cee25259fe9720e790fc0fd2b50845eb5e
yufen-lin <ruby.yf.lin@gmail.com>
2024-11-29 21:21:42 +0800

    Complete Question 3

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -222,14 +222,197 @@ FROM
 -- 1. 將用戶`李燕容`新增為教練，並且年資設定為2年（提示：使用`李燕容`的email ，取得 `李燕容` 的 `id` ）
 -- 2. 將用戶`肌肉棒子`新增為教練，並且年資設定為2年
 -- 3. 將用戶`Q太郎`新增為教練，並且年資設定為2年
+INSERT INTO
+    "COACH" (user_id, experience_years)
+VALUES
+    (
+        (
+            SELECT
+                id
+            FROM
+                "USER"
+            WHERE
+                email = 'lee2000@hexschooltest.io'
+        ),
+        2
+    ),
+    (
+        (
+            SELECT
+                id
+            FROM
+                "USER"
+            WHERE
+                email = 'muscle@hexschooltest.io'
+        ),
+        2
+    ),
+    (
+        (
+            SELECT
+                id
+            FROM
+                "USER"
+            WHERE
+                email = 'starplatinum@hexschooltest.io'
+        ),
+        2
+    );
+
 -- 3-2. 新增：承1，為三名教練新增專長資料至 `COACH_LINK_SKILL` ，資料需求如下：
 -- 1. 所有教練都有 `重訓` 專長
+INSERT INTO
+    "COACH_LINK_SKILL" (coach_id, skill_id)
+SELECT
+    c.id AS coach_id,
+    (
+        SELECT
+            id
+        FROM
+            "SKILL"
+        WHERE
+            name = '重訓'
+    ) AS skill_id
+FROM
+    "COACH" c;
+
 -- 2. 教練`肌肉棒子` 需要有 `瑜伽` 專長
+INSERT INTO
+    "COACH_LINK_SKILL" (coach_id, skill_id)
+VALUES
+    (
+        (
+            SELECT
+                id
+            FROM
+                "COACH" c
+            WHERE
+                c.user_id = (
+                    SELECT
+                        id
+                    FROM
+                        "USER"
+                    WHERE
+                        email = 'muscle@hexschooltest.io'
+                )
+        ),
+        (
+            SELECT
+                id
+            FROM
+                "SKILL"
+            WHERE
+                name = '瑜伽'
+        )
+    );
+
 -- 3. 教練`Q太郎` 需要有 `有氧運動` 與 `復健訓練` 專長
+INSERT INTO
+    "COACH_LINK_SKILL" (coach_id, skill_id)
+VALUES
+    (
+        (
+            SELECT
+                id
+            FROM
+                "COACH" c
+            WHERE
+                c.user_id = (
+                    SELECT
+                        id
+                    FROM
+                        "USER"
+                    WHERE
+                        email = 'starplatinum@hexschooltest.io'
+                )
+        ),
+        (
+            SELECT
+                id
+            FROM
+                "SKILL"
+            WHERE
+                name = '有氧運動'
+        )
+    ),
+    (
+        (
+            SELECT
+                id
+            FROM
+                "COACH" c
+            WHERE
+                c.user_id = (
+                    SELECT
+                        id
+                    FROM
+                        "USER"
+                    WHERE
+                        email = 'starplatinum@hexschooltest.io'
+                )
+        ),
+        (
+            SELECT
+                id
+            FROM
+                "SKILL"
+            WHERE
+                name = '復健訓練'
+        )
+    );
+
+-- 驗證 3-2 結果
+/*SELECT u.name, s.name
+FROM "COACH_LINK_SKILL" cls 
+JOIN "SKILL" s ON s.id = cls.skill_id
+JOIN "COACH" c ON c.id = cls.coach_id
+JOIN "USER" u ON u.id = c.user_id
+ORDER BY s.name ASC;*/
 -- 3-3 修改：更新教練的經驗年數，資料需求如下：
 -- 1. 教練`肌肉棒子` 的經驗年數為3年
+UPDATE "COACH"
+SET
+    experience_years = 3
+WHERE
+    user_id = (
+        SELECT
+            id
+        FROM
+            "USER"
+        WHERE
+            email = 'muscle@hexschooltest.io'
+    );
+
 -- 2. 教練`Q太郎` 的經驗年數為5年
+UPDATE "COACH"
+SET
+    experience_years = 5
+WHERE
+    user_id = (
+        SELECT
+            id
+        FROM
+            "USER"
+        WHERE
+            email = 'starplatinum@hexschooltest.io'
+    );
+
+-- 驗證 3-3 結果
+/*SELECT u."name", c.experience_years 
+FROM "COACH" c
+JOIN "USER" u ON c.user_id = u.id;*/
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
+INSERT INTO
+    "SKILL" (name)
+VALUES
+    ('空中瑜伽');
+
+DELETE FROM "SKILL"
+WHERE
+    name = '空中瑜伽';
+
+-- 驗證 3-4 結果
+-- SELECT * FROM "SKILL";
 --  ████████  █████   █    █   █ 
 --    █ █   ██    █  █     █   █ 
 --    █ █████ ███ ███      █████ 
